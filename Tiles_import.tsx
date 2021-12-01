090fa14e23f60bd5866874505dcfb233fb11a77d
AquarieChan <aquarie.chan@gmail.com>
2021-12-01 13:31:59 +0100

    fix

--- Tiles_import.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Tiles" tilewidth="32" tileheight="32" tilecount="45" columns="9">
- <image source="../../../Downloads/CuteRPG/CuteRPG/Tiles.png" width="288" height="176"/>
-</tileset>
